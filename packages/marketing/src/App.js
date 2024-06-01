12d53fd76cd628660d61a45046c287b8a5e745d3
FCI <fci@FCIs-MacBook-Pro.local>
2024-06-02 00:01:56 +0530

    Initial commit

---
+++ packages/marketing/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { StylesProvider } from "@material-ui/core/styles";
+
+import Album from "./components/Landing";
+import Pricing from "./components/Pricing";
+
+export default () => {
+    return <div>
+        <StylesProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/pricing" component={Pricing} />
+                    <Route path="/" component={Album} />
+                </Switch>
+            </BrowserRouter>
+        </StylesProvider>
+    </div>
+}
