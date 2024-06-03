be92cb592c95ad6c16eff98fed96ae5bf8321891
FCI <fci@FCIs-MacBook-Pro.local>
2024-06-03 10:51:46 +0530

    added missing config for auth in webpack dev and prod

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -16,7 +16,8 @@ const devConfig = {
         new ModuleFederationPlugin({
             name: "Container",
             remotes: {
-                marketing: "marketing@http://localhost:8081/remoteEntry.js"
+                marketing: "marketing@http://localhost:8081/remoteEntry.js",
+                auth: "auth@http://localhost:8082/remoteEntry.js",
             },
             shared: packageJson.dependencies
         })
--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,8 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: "Container",
             remotes: {
-                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+                auth: `auth@${domain}/auth/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies,
         })
