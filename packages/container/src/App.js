d525d5c66bb1970faafd65bc632e4a3e806a54f1
FCI <fci@FCIs-MacBook-Pro.local>
2024-06-03 12:30:09 +0530

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,34 +1,47 @@
 import React, { lazy, Suspense } from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import Progress from "./components/Progress";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { createBrowserHistory } from "history";
 
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co"
-})
+});
+
+const history = createBrowserHistory();
 
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push("/dashboard")
+        }
+    }, [isSignedIn])
     return (
-    <BrowserRouter>
-        <StylesProvider generateClassName={generateClassName}>
-            <div>
-                <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
-                <Suspense fallback={<Progress />}>
-                    <Switch>
-                        <Route path="/auth">
-                            <AuthApp onSignIn={() => setIsSignedIn(true)} />
-                        </Route>
-                        <Route path="/" component={MarketingApp} />
-                    </Switch>
-                </Suspense>
-            </div>
-        </StylesProvider>
-    </BrowserRouter>
+        <Router history={history}>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthApp onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/" />}
+                                <DashboardApp />
+                            </Route>
+                            <Route path="/" component={MarketingApp} />
+                        </Switch>
+                    </Suspense>
+                </div>
+            </StylesProvider>
+        </Router>
     )
 }
